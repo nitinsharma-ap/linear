246ba347e1b804f3e8ab78645b0b726d7340562f
Nitn Sharma <nitinsharma98393139@gmail.com>
2024-07-15 12:56:38 +0530

    change data

--- src/Redux/Reducer/rootReducer.js
+++ src/Redux/Reducer/rootReducer.js
@@ -1,16 +1,15 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import subReducer from './subReducer';
-import Register from './Rgister';
+
 import themeReducer from './theamSlice';
 import tasksReducer from './taskReducer';
-import DataReducer from './DataReducer';
+
 
 const rootReducer = combineReducers({
      subReducer,
-     Register,
     themeReducer,
     tasksReducer,
-    DataReducer
+
 }
 
 );
